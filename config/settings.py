ac0a452a633eab2dc467b813874803e5becd4acf
snova <awaawapurin@gmail.com>
2020-05-30 23:50:52 +0900

    updated

--- config/settings.py
+++ config/settings.py
@@ -22,6 +22,7 @@ ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 # Application definition
 
 INSTALLED_APPS = [
+    'afterglow.apps.AfterglowConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
