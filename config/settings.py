fd34f1eec846b8a617fd3d6347eb825c7f4bdb2d
AWAx2 <awaawapurin@gmail.com>
2020-06-11 16:57:12 +0900

    updated

--- config/settings.py
+++ config/settings.py
@@ -2,11 +2,6 @@ import os
 import environ
 
 
-try:
-    from .local_settings import *
-except ImportError:
-    pass
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
